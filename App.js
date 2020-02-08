8718cf6be552dd851cbf35981c2a547be9760439
lewisf20 <lewis_frater@yahoo.com>
2020-02-08 01:12:43 +0000

    Added login - works, contacts server

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 
 import AppNavigator from './navigation/AppNavigator';
+import Login from './components/Login';
 
 export default function App() {
   return(
-    <AppNavigator /> 
+    <Login />
   );
 }
---
+++ components/Login.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import { Text, TextInput, View, Button, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        //components state
+        this.state = {
+            email: '',
+            password: '',
+        };
+        this.login = this.login.bind(this);
+    }
+
+
+    /**{
+  "given_name": "Lewis",
+  "family_name": "Frater",
+  "email": "LewisFrater@gmail.com",
+  "password": "lewis1234"
+} */
+    login() {
+        return fetch('http://10.0.2.2:3333/api/v0.0.4/login',
+            {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: this.state.email,
+                    password: this.state.password
+                })
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+
+    render() {
+        return (
+            <View style={styles.screen}>
+                <Text style={styles.header}>Chittr Login</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="email..."
+                    onChangeText={(email) => this.setState({ email })}
+                    value={this.state.email}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="password..."
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({ password })}
+                    value={this.state.password}
+                />
+
+                <TouchableOpacity
+                    style={styles.button}
+                    title="Log in"
+                    onPress={() => this.login()}
+                ><Text style={styles.btnText}>Log in</Text>
+                </TouchableOpacity>
+
+            </View>
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor: "#cbcbcb",
+    },
+    input: {
+        backgroundColor: "#fff",
+        fontSize: 24,
+        margin: 5,
+        textAlign: "center",
+        borderBottomWidth: 5,
+        borderColor: "skyblue"
+    },
+    button: {
+        backgroundColor: "skyblue",
+        width: "50%",
+        height: 50,
+        alignSelf: "center",
+        justifyContent: "center",
+        borderWidth: 3,
+        borderColor: "#fff",
+
+    },
+    btnText: {
+        textAlign: "center",
+        color: "#fff",
+        fontSize: 28,
+        fontWeight: "bold",
+    },
+    header: {
+        textAlign: "center",
+        color: "royalblue",
+        fontSize: 44
+    }
+});
+
+export default Login; 
