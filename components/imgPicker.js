8fc13d64c8880dde9d3c6861d35d5c3e829b44de
lewisf20 <lewis_frater@yahoo.com>
2020-03-14 01:49:32 +0000

    test commit- used backup as messed app up

--- components/imgPicker.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-
-const imgPicker = props => {};
-
-const styles = StyleSheet.create({});
-
-export default imgPicker;
