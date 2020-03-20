f9afe42dfa4e7e707f7c366f224e4bd13725d9fa
lewisf20 <lewis_frater@yahoo.com>
2020-03-20 02:39:40 +0000

    Draft functionality completed

--- screens/ViewDrafts.js
+++ screens/ViewDrafts.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, FlatList, AsyncStorage} from 'react-native';
 
 import {useSelector, useDispatch} from 'react-redux';
@@ -11,38 +11,73 @@ import Color from '../constants/Colors';
 
 const ViewDrafts = props => {
   const dispatch = useDispatch();
-  const drafts = props.navigation.getParam('drafts');
+  const [chitDrafts, setChitDrafts] = useState([]);
   const location = props.navigation.getParam('location');
   const storeToken = useSelector(state => state.authentication.token);
-  const [chitText, setChitText] = useState('');
+
+  const getAllChitDrafts = async () => {
+    try {
+      let value = await AsyncStorage.getItem('drafts');
+      let jsonValue = JSON.parse(value);
+      setChitDrafts(jsonValue);
+      console.log(jsonValue);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getAllChitDrafts();
+  }, [removeFromDrafts]);
 
   const postChit = async chit => {
     try {
-      await dispatch(chitActions.postChit(storeToken, chitText, location));
+      await dispatch(chitActions.postChit(storeToken, chit, location));
       props.navigation.navigate('Home');
     } catch (err) {
       console.log(err);
     }
   };
 
-  const postChitHandler = () => {
-    postChit();
+  const postChitHandler = chit => {
+    postChit(chit);
+  };
+
+  const removeFromDrafts = async chit => {
+    try {
+      const value = chitDrafts;
+      const newValue = value.filter(theChit => theChit !== chit);
+      setChitDrafts(newValue);
+      const stringify = JSON.stringify(newValue);
+      await AsyncStorage.setItem('drafts', stringify);
+      console.log('new val = ' + newValue);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const renderDraftItem = itemData => {
     return (
       <Card style={styles.draftItem}>
-        <Text style={styles.textItem}>{itemData.item}</Text>
+        <Text style={styles.textItem}>{itemData.item.toString()}</Text>
         <View style={styles.btnContainer}>
           <Btn
             title="Post"
             style={styles.buttons}
             onPress={() => {
-              setChitText(itemData.item.toString());
-              postChitHandler();
+              let theChit = itemData.item.toString();
+              console.log('chit = ' + theChit);
+              postChitHandler(theChit);
             }}
           />
-          <Btn title="Delete" style={styles.buttons} />
+          <Btn
+            title="Delete"
+            style={styles.buttons}
+            onPress={() => {
+              let theChit = itemData.item.toString();
+              removeFromDrafts(theChit);
+            }}
+          />
         </View>
       </Card>
     );
@@ -50,11 +85,11 @@ const ViewDrafts = props => {
   return (
     <View style={styles.container}>
       <Text>View Drafts Screen</Text>
-      {console.log('Draft screen = ' + drafts)}
+      {console.log('Draft screen = ' + chitDrafts)}
       <FlatList
         contentContainerStyle={styles.list}
         keyExtractor={item => item.toString()}
-        data={drafts}
+        data={chitDrafts}
         renderItem={renderDraftItem}
       />
     </View>
